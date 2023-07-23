e76df6a16324a9180b25c7b34244c08d9449606d
nikkocai <nicolas_salvanescki@hotmail.com>
2023-07-23 08:27:17 -0300

    Funciona, pero no es MVVM estrictamente

---
+++ app/src/main/java/com/curso/android/app/practica/proyectofinal/view/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.curso.android.app.practica.proyectofinal.view
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.curso.android.app.practica.proyectofinal.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mainViewModel.result.observe(this) {
+            binding.resultOutput.text = if (it.cmp) "Son iguales" else "No son iguales"
+        }
+
+        binding.cmpBtn.setOnClickListener{
+            mainViewModel.compareTextFields(
+                binding.textField1.text.toString(),
+                binding.textField2.text.toString()
+            )
+        }
+
+    }
+}
